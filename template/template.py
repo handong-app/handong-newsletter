f1d6be3e324069ce4a61e2dd4cb2f14b4ed02c60
junglesub <junglesubmarine@gmail.com>
2022-01-24 00:40:59 +0900

    bracket error and conditions

--- template/template.py
+++ template/template.py
@@ -5,11 +5,12 @@ def render_html():
   with open("template/template.json", encoding="utf8") as dummy_file:
     dummy = json.load(dummy_file)
     FOODDATA = dummy["food"]
+    ANON_HOTDATA = dummy["anon"]["hot5"]
 
   env = Environment(
     loader=FileSystemLoader(".")
   )
   template = env.get_template("template/index.jinja")
 
-  rendered_html = template.render(name="Jungsub", food=FOODDATA)
+  rendered_html = template.render(name="Jungsub", food=FOODDATA, anon_hot=ANON_HOTDATA)
   return rendered_html
