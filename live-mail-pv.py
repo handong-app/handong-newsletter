c63ebcc9a6863a32d4c1e4e67c4b6802c2458a08
junglesub <junglesubmarine@gmail.com>
2022-01-24 00:01:06 +0900

    change .html to .jinja

--- live-mail-pv.py
+++ live-mail-pv.py
@@ -5,14 +5,14 @@ def buildMail():
   env = Environment(
     loader=FileSystemLoader(".")
   )
-  template = env.get_template("template/index.html")
+  template = env.get_template("template/index.jinja")
 
   rendered_html = template.render(name="Jungsub")
-  with open("build/index.html", "w", encoding="utf-8") as file:
+  with open("build/index.jinja", "w", encoding="utf-8") as file:
     file.write(rendered_html)
   
 buildMail()
 
 server = Server()
-server.watch('template/*.html', buildMail)
+server.watch('template/*.jinja', buildMail)
 server.serve(root='build/')
