192b1ecbb8ac5745ca03e680506fb9e5cee8ff28
junglesub <junglesubmarine@gmail.com>
2022-01-23 22:54:56 +0900

    convert to inline-css before sending

--- sendmail.py
+++ sendmail.py
@@ -1,5 +1,6 @@
 import os
 import smtplib, ssl
+from premailer import transform
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
@@ -8,21 +9,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 메일 준비하기
-htmlFile = open("newsletter.html", "rb")
-htmlBody = MIMEText(htmlFile.read(), "html", _charset='UTF-8')
+htmlFile = open("newsletter.html", "r", encoding="utf-8")
+htmlBody = htmlFile.read()
+htmlMIME = MIMEText(htmlBody, "html")
 htmlFile.close()
+
+# print(transform(htmlBody))
 
 msg = MIMEMultipart("alternative")
 msg["Subject"] = "This is Subject"
 msg["From"] = os.getenv("EMAIL_SENDER")
 msg["To"] = "junglesubmarine@gmail.com"
-msg.attach(htmlBody)
+msg.attach(htmlMIME)
 
-smtp = smtplib.SMTP('localhost')
-smtp.sendmail("test@example.com", "junglesubmarine@gmail.com", msg.as_string())
+# smtp = smtplib.SMTP('localhost')
+# smtp.sendmail("test@example.com", "junglesubmarine@gmail.com", msg.as_string())
 
 # 메일 보내기
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(os.getenv("SMTP_HOST"), context=context) as smtp:
-#   smtp.login(os.getenv("SMTP_ID"), os.getenv("SMTP_PW")) # 아이디 비밀번호로 로그인
-#   smtp.sendmail(msg["From"], msg["To"], msg.as_string())
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(os.getenv("SMTP_HOST"), context=context) as smtp:
+  smtp.login(os.getenv("SMTP_ID"), os.getenv("SMTP_PW")) # 아이디 비밀번호로 로그인
+  smtp.sendmail(msg["From"], msg["To"], msg.as_string())
