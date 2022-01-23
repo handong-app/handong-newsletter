3950f759cebf60c554491a38cfc0913b7a917499
junglesub <junglesubmarine@gmail.com>
2022-01-23 19:27:53 +0900

    Developing

---
+++ DEVELOPMENT.md
@@ -0,0 +1,5 @@
+## 의존성 라이브러리 설치
+
+```
+pip install -r requirements.txt
+```
---
+++ sendmail.py
@@ -0,0 +1,28 @@
+import os
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+# 시스템 변수 불러오기
+load_dotenv()
+
+# 메일 준비하기
+htmlFile = open("newsletter.html", "rb")
+htmlBody = MIMEText(htmlFile.read(), "html", _charset='UTF-8')
+htmlFile.close()
+
+msg = MIMEMultipart("alternative")
+msg["Subject"] = "This is Subject"
+msg["From"] = os.getenv("EMAIL_SENDER")
+msg["To"] = "junglesubmarine@gmail.com"
+msg.attach(htmlBody)
+
+smtp = smtplib.SMTP('localhost')
+smtp.sendmail("test@example.com", "junglesubmarine@gmail.com", msg.as_string())
+
+# 메일 보내기
+# context = ssl.create_default_context()
+# with smtplib.SMTP_SSL(os.getenv("SMTP_HOST"), context=context) as smtp:
+#   smtp.login(os.getenv("SMTP_ID"), os.getenv("SMTP_PW")) # 아이디 비밀번호로 로그인
+#   smtp.sendmail(msg["From"], msg["To"], msg.as_string())
