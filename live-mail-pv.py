17f50fd4ce436e91c8ad0c427161fb27b29f212b
junglesub <junglesubmarine@gmail.com>
2022-02-22 00:48:39 +0900

    fix: create build folder if not exists

--- live-mail-pv.py
+++ live-mail-pv.py
@@ -1,8 +1,11 @@
 from livereload import Server, shell
 from template.template import render_html
+import os
 
 
 def buildMail():
+  if not os.path.exists("build"):
+    os.makedirs("build")
   with open("build/index.html", "w", encoding="utf-8") as file:
     file.write(render_html())
   
