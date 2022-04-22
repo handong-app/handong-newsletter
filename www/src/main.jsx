1e56fbd0981751d832742aab2eb3c14b6f92cc2a
junglesub <junglesubmarine@gmail.com>
2022-04-23 00:05:53 +0900

    feat: www redirect /form to google form

--- www/src/main.jsx
+++ www/src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+console.log(window.location.pathname);
+if (window.location.pathname === "/form") {
+  window.location.replace("https://forms.gle/uJEA4jnao35YAeYR9");
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
